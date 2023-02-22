cb35ad8c5e441249511a3798452d21807ea472fa
Nicole Henríquez <ale.nicolehs@gmail.com>
2023-02-22 18:31:50 -0300

    se agrega función para discriminar por extensión .md

--- funciones.js
+++ funciones.js
@@ -2,18 +2,20 @@ const fs = require('fs');
 const path = require('path');
 
 //----------TO DO LIST----------
-//1. La ruta es absoluta? (transformarla a absoluta)
+
+//1. La ruta es absoluta?
 const isAbsolute = (pathInput) => {
-    return path.isAbsolute(pathInput) ? true : false;
+    let rutaAbsoluta = path.isAbsolute(pathInput);
+    return rutaAbsoluta ? true : false;
 };
 
 //1.1 transformar ruta relativa a absoluta
 const relativeToAbsolute = (pathInput) => {
-    return path.resolve(process.cwd(), pathInput);
+    return path.resolve(process.cwd(), pathInput); //retorna la ruta absoluta
 };
 
 //2. La ruta existe en la computadora?
-const realPath = () => {
+const realPath = (pathInput) => {
     const pathExist = fs.existsSync(pathInput);
     return pathExist ? true : false;
 };
@@ -21,9 +23,28 @@ const realPath = () => {
 //3. Es un archivo?
 
 //4. Es un archivo .md?
+const isMdFile = (pathInput) => {
+    let extFile = path.extname(pathInput);
+return (extFile === '.md') ? true : false;
+};
+
+console.log(isMdFile('index.js'));
+
 //5. Es un directorio? (recursividad para llegar a comprobar c/ruta dentro) Sincrono
+//let files = fs.readdirSync(pathInput)
+
+
+/*---------Para concatenar URLs se utiliza path.join('pathDIR', 'fileBasename) */
+
 //6. Almacenar todos los .md en un array de archivos md
+
 //7. Leer archivos md en busca de links (utilizar exreg) Asincrono
+// fs.readFile(pathInput, 'utf-8', (error, archivo) => {
+//     if(error){
+//         throw error;
+//     }
+//     console.log(archivo);
+// });
 //8. Guardar todos los links en un array 
 //-----API------
 //9. Desea validad los links? Desea estadísticas? imprimir la info por defecto (array de todos los links encontrados)
@@ -33,34 +54,52 @@ const realPath = () => {
 //-----CLI------
 //12. 
 
+//const mdLinks = (pathInput) => {
+    // if(isAbsolute){
+    //     console.log('La ruta: ' + pathInput + ' ingresada es absoluta')
+        // if(realPath){
+        //     return pathInput;
+        // }else{
+        //     console.log('La ruta: ' + pathInput + ' no existe en esta computadora');
+        // }
+    // }else{
+    //     console.log('La ruta: ' + pathInput + ' ingresada es relativa')
+        // let rutaResuelta = relativeToAbsolute(pathInput);
 
+        // if(realPath){
+        //    return rutaResuelta;
+        // }else{
+        //     console.log('La ruta: ' + rutaResuelta + ' no existe en esta computadora')
+        // }
+//     }
+// }
 //función para validar el Path (requiere el path)
 
-function validationPath(pathInput) { //la ruta es válida?
-    if (pathInput == undefined) {
-        return undefined;
-    }
-    else if (path.isAbsolute(pathInput)) {
-        console.log('La ruta: ' + pathInput + ' ingresada es absoluta')
-        if(fs.existsSync(pathInput)){
-            return pathInput
+// function validationPath(pathInput) { //la ruta es válida?
+//     if (pathInput == undefined) {
+//         return undefined;
+//     }
+//     else if (path.isAbsolute(pathInput)) {
+//         console.log('La ruta: ' + pathInput + ' ingresada es absoluta')
+//         if(fs.existsSync(pathInput)){
+//             return pathInput
 
-        }else{
-            console.log('La ruta: ' + pathInput + ' no existe en esta computadora');
-        }
-    }else {
-        console.log('La ruta: ' + pathInput + ' ingresada es relativa');
-        let rutaResuelta = path.resolve(process.cwd(), pathInput);
-        console.log('Ruta resuelta: ' + rutaResuelta);
-        if(fs.existsSync(rutaResuelta)){
-            return rutaResuelta
-        }else{
-            console.log('La ruta: ' + rutaResuelta + ' no existe en esta computadora')
-        }
+//         }else{
+//             console.log('La ruta: ' + pathInput + ' no existe en esta computadora');
+//         }
+//     }else {
+//         console.log('La ruta: ' + pathInput + ' ingresada es relativa');
+//         let rutaResuelta = path.resolve(process.cwd(), pathInput);
+//         console.log('Ruta resuelta: ' + rutaResuelta);
+//         if(fs.existsSync(rutaResuelta)){
+//             return rutaResuelta
+//         }else{
+//             console.log('La ruta: ' + rutaResuelta + ' no existe en esta computadora')
+//         }
 
-    }
-};
+//     }
+// };
 
-validationPath('/Users/tsukito/Library/CloudStorage/OneDrive-Personal/LABORATORIA/mdlinks/DEV002-md-links/RADME.md');
+//console.log(realPath(relativeToAbsolute('RADME.md')));
 
-module.exports = validationPath;
+//module.exports = isAbsolute;
--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const validationPath = require("./funciones");
+
 module.exports = () => {
+  mdLinks
   // ...
 };
+
+
+function mdLinks(path, options){
+  if(options.validate){
+console.log('estoy validado!!!');
+  }else{
+    console.log('no estoy validado');
+  }
+  console.log(path);
+  console.log(options);
+
+};
+
+mdLinks('/Users/tsukito/Library/CloudStorage/OneDrive-Personal/LABORATORIA/mdlinks/DEV002-md-links/README.md', options.validate);
