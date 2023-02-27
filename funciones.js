561f0fe4e756a5f94b72b75684cc830e939dd78c
Nicole Henríquez <ale.nicolehs@gmail.com>
2023-02-27 16:46:46 -0300

    se modifca JSON para testear, se crea archivo funciones.spec.js, se agregan los test de las 9 funciones de funciones.js

--- funciones.js
+++ funciones.js
@@ -31,7 +31,7 @@ const isAFile = (pathInput) => {
 //4. Es un archivo .md?
 const isMdFile = (pathInput) => {
     let extFile = path.extname(pathInput);
-return (extFile === '.md') ? true : false;
+    return (extFile === '.md') ? true : false;
 };
 
 
@@ -42,9 +42,9 @@ return (extFile === '.md') ? true : false;
 
 //5. Es un directorio? (recursividad para llegar a comprobar c/ruta dentro) Sincrono
 const isADirectory = (pathInput) => {
-   let stats = fs.statSync(pathInput);
-   let dir = stats.isDirectory();
-   return dir ? true : false;
+    let stats = fs.statSync(pathInput);
+    let dir = stats.isDirectory();
+    return dir ? true : false;
 };
 
 
@@ -56,50 +56,75 @@ const readDir = (pathInput) => {
     return fs.readdirSync(pathInput);
 };
 
-//7. Almacenar todos los .md en un array de archivos md
+//7. Almacenar todos los .md en un array de archivos md (se aplica la recursividad con createMdArray)
 
-let mdArray = [];
-function arrayMdCreate (pathInput){
-
-    if(isAFile(pathInput) && isMdFile(pathInput)){
-mdArray.push(pathInput);
-    }else if (isADirectory(pathInput)){
-        return 'es un directorio, no se puede leer aun';
-
+function createMdArray(pathInput) {
+    let mdArray = []; //debo colocarlo dentro de la función para que no se dupliquen los archivos dentro del array
+    if (isAFile(pathInput) && isMdFile(pathInput)) {
+        mdArray.push(pathInput);
+    } else if (isADirectory(pathInput)) {
+        const filesDirArray = readDir(pathInput);
+        filesDirArray.map((filesDirArray) => {
+            mdArray = mdArray.concat(createMdArray(`${pathInput}/${filesDirArray}`));
+        });
     }
     return mdArray;
 };
 
-console.log(readDir('README.md'))
-//console.log(mdArray)
-
-//7. Leer archivos md en busca de links (utilizar exreg) Asincrono
-
-// function readMdFile(pathInput){
-// const promise = new Promise (function(resolve, reject){
-// if(isMdFile){
-//     let archivo = fs.readFile(pathInput, 'utf-8', (error, archivo) => {
-//         if(error){
-//             throw error;
-//         }
-//         console.log(archivo);
-//     });
-//     resolve()
-// }else{
-//     reject(`no es un archivo .md`)
-// }
-// });
-// }
+//console.log(createMdArray('/Users/tsukito/Library/CloudStorage/OneDrive-Personal/LABORATORIA/mdlinks/DEV002-md-links'))
 
 
-//8. Guardar todos los links en un array 
+//8. Leer archivos md en busca de links (utilizar exreg) Asincrono
+
+//podría comenzar leyendo el o los archivos (mdArray) Async transformandolo/s en un array con split ' '
+//Luego aplicar un filter por la urlExpReg, así se obtiene el arrayde URLS
+
+//8.1 leer archivos de forma asincrona(promesas)
+
+const readMdFile = (pathInput) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(pathInput, 'utf-8', (error, archivo) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(archivo);
+        });
+    });
+};
+
+console.log(readMdFile('markdownFiles/archivo.txt'))
+
+//8.2 obrener los links 
+const createLinkArray = (pathInput) => {
+    return new Promise ((resolve, reject) => {
+        let LinkArray = [];
+        readMdFile(pathInput)
+            .then((data) => {
+                const urlExpReg = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+                // let fileArray = data.split(' ');
+                // console.log(fileArray);//las url no están separadas por espacio en algunos casos, por lo que no sirve split(' ')
+                // let links = fileArray.filter(urlExpReg);
+                //console.log(data)
+                let link = urlExpReg.exec(data);
+                while(link !== null){
+                    LinkArray.push(link);
+                    link =urlExpReg.exec(data);
+                }
+                resolve(LinkArray);
+            })
+            .catch((error)=> reject(error));
+    });
+};
+//console.log(createLinkArray('/Users/tsukito/Library/CloudStorage/OneDrive-Personal/LABORATORIA/mdlinks/DEV002-md-links/markdownFiles/markdownLinks.md'))
+
+//9. Guardar todos los links en un array 
 //-----API------
-//9. Desea validad los links? Desea estadísticas? imprimir la info por defecto (array de todos los links encontrados)
-//10. --validate:true, realizar la petición HTTP mediante fetch() para obtener estado y codigo
-//11. --status, a través de lo obtenido mediante la validación, arrojar links totales y unicos
-//11.1 se debe hacer una función para eliminar los links repetidos (puede ser un array que no permita dos elementos iguales)
+//10. Desea validad los links? Desea estadísticas? imprimir la info por defecto (array de todos los links encontrados)
+//11. --validate:true, realizar la petición HTTP mediante fetch() para obtener estado y codigo
+//12. --status, a través de lo obtenido mediante la validación, arrojar links totales y unicos
+//12.1 se debe hacer una función para eliminar los links repetidos (puede ser un array que no permita dos elementos iguales)
 //-----CLI------
-//12. 
+//13. 
 
 //const mdLinks = (pathInput) => {
 
@@ -151,4 +176,14 @@ console.log(readDir('README.md'))
 
 //console.log(realPath(relativeToAbsolute('RADME.md')));
 
-//module.exports = isAbsolute;
+module.exports = {
+    isAbsolute,
+    relativeToAbsolute,
+    realPath,
+    isAFile,
+    isMdFile,
+    isADirectory,
+    createMdArray,
+    readMdFile,
+    createLinkArray
+};
---
+++ test/funciones.spec.js
@@ -0,0 +1,108 @@
+const { isAbsolute, relativeToAbsolute, realPath, isAFile, isMdFile, isADirectory, createMdArray, readMdFile, createLinkArray } = require('../funciones.js');
+
+//RUTAS para prueba
+const archivoNoMd = 'index.js';
+const archivoMd = 'README.md';
+const directorio = 'markdownFiles';
+const rutaAbsoluta = '/Users/tsukito/Library/CloudStorage/OneDrive-Personal/LABORATORIA/mdlinks/DEV002-md-links/markdownFiles/markdownLinks.md';
+const rutaNoExiste = 'Users/tsukito/Library/CloudStorage/OneDrive-Personal/LABORATORIA/mdlinks/DEV002-md-links/markdownFiles/markdownLinks.md';
+const rutaRelativa = 'markdownFiles/markdownLinks.md';
+const archivoTexto = '/Users/tsukito/Library/CloudStorage/OneDrive-Personal/LABORATORIA/mdlinks/DEV002-md-links/markdownFiles/archivo.txt';
+
+
+
+describe('isAbsolute', () => {
+    it('debe devolver true si la ruta es absoluta', () => {
+        isAbsolute(rutaAbsoluta);
+        expect(isAbsolute(rutaAbsoluta)).toEqual(true);
+    });
+    it('debe devolver false si la ruta es relativa', () => {
+        isAbsolute(rutaAbsoluta);
+        expect(isAbsolute(rutaRelativa)).toEqual(false);
+    });
+});
+
+describe('relativeToAbsolute', () => {
+    it('debe transformar la ruta relativa en absoluta', () => {
+        relativeToAbsolute(rutaRelativa);
+        expect(relativeToAbsolute(rutaRelativa)).toEqual(rutaAbsoluta);
+    });
+});
+
+describe('realPath', () => {
+    it('debe devolver true si la ruta no existe en la PC', () => {
+        realPath(rutaAbsoluta);
+        expect(realPath(rutaAbsoluta)).toEqual(true);
+    });
+    it('debe devolver false si la ruta existe en la PC', () => {
+        realPath(rutaNoExiste);
+        expect(realPath(rutaNoExiste)).toEqual(false);
+    });
+    it('debe devolver true si la ruta relativa existe en la PC', () => {
+        realPath(rutaRelativa);
+        expect(realPath(rutaRelativa)).toEqual(true);
+    });
+});
+
+describe('isAFile', () => {
+    it('debe devolver true si la ruta corresponde a un archivo', () => {
+        isAFile(archivoMd);
+        expect(isAFile(archivoMd)).toEqual(true);
+    });
+    it('debe devolver false si la ruta no corresponde a un archivo', () => {
+        isAFile(directorio);
+        expect(isAFile(directorio)).toEqual(false);
+    });
+});
+
+describe('isMdFile', () => {
+    it('debe devolver true si es un archivo con extensión .md', () => {
+        isMdFile(archivoMd);
+        expect(isMdFile(archivoMd)).toEqual(true);
+    });
+    it('debe devolver false si no es un archivo con extensión .md', () => {
+        isMdFile(archivoNoMd);
+        expect(isMdFile(archivoNoMd)).toEqual(false);
+    });
+});
+
+describe('isADirectory', () => {
+    it('debe devolver true si la ruta corresponde a un directorio', () => {
+        isADirectory(directorio);
+        expect(isADirectory(directorio)).toEqual(true);
+    });
+    it('debe devolver false si la ruta no corresponde a un directorio', () => {
+        isADirectory(archivoMd);
+        expect(isADirectory(archivoMd)).toEqual(false);
+    });
+});
+
+describe('createMdArray', () => {
+    it('debe retornar un array de archivos .md', () => {
+        createMdArray(directorio);
+        expect(createMdArray(directorio)).toEqual(["markdownFiles/markdownLinks.md", "markdownFiles/markdownNolinks.md"]);
+
+    });
+});
+
+describe('readMdFile', () => {
+    it('debe ser una función', () => {
+        expect(typeof readMdFile).toBe('function');
+    });
+    it('debe devolver una promesa', () => readMdFile()
+        .then(() => {
+            expect(readMdFile).toBe(typeof 'promise');
+        })
+        .catch((error) => error));
+});
+
+describe('createLinkArray', () => {
+    it('debe ser una función', () => {
+        expect(typeof createLinkArray).toBe('function');
+    });
+    it('debe devolver una promesa', () => createLinkArray()
+        .then(() => {
+            expect(createLinkArray).toBe(typeof 'promise');
+        })
+        .catch((error) => error));
+});
